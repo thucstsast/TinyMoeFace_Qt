5ca3ef0e1d12ab77e5c8a047b14df8040ad5213d
Tan Si Nan <tansinan_1995@163.com>
2017-02-11 14:37:51 +0800

    Add CGAL library.

---
+++ CGALTriangulator.cpp
@@ -0,0 +1,13 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Constrained_Delaunay_triangulation_2.h>
+#include <CGAL/Delaunay_mesher_2.h>
+#include <CGAL/Delaunay_mesh_face_base_2.h>
+#include <CGAL/Delaunay_mesh_size_criteria_2.h>
+#include <iostream>
+
+#include "CGALTriangulator.h"
+
+CGALTriangulator::CGALTriangulator()
+{
+
+}
---
+++ CGALTriangulator.h
@@ -0,0 +1,11 @@
+#ifndef CGALTRIANGULATOR_H
+#define CGALTRIANGULATOR_H
+
+
+class CGALTriangulator
+{
+public:
+    CGALTriangulator();
+};
+
+#endif // CGALTRIANGULATOR_H
