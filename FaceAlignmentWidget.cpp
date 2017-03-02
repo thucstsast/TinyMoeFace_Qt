5306e33b2a6d0bcaa51f65983f4173ff9a08fabe
Tan Si Nan <tansinan_1995@163.com>
2017-03-02 20:58:13 +0800

    Use FaceX to perform face alignment in Qt.

--- FaceAlignmentWidget.cpp
+++ FaceAlignmentWidget.cpp
@@ -1,5 +1,6 @@
 #include <QCamera>
 #include <QPainter>
+#include "face_x.h"
 #include "FaceAlignmentWidget.h"
 
 FaceAlignmentWidget::FaceAlignmentWidget(QWidget *parent) : QWidget(parent)
@@ -7,12 +8,20 @@ FaceAlignmentWidget::FaceAlignmentWidget(QWidget *parent) : QWidget(parent)
     setMinimumSize(100, 100);
     simpleCameraSurface = new SimpleCameraSurface(this);
     connect(simpleCameraSurface, SIGNAL(frameAvailable(QImage)), this, SLOT(onFrameAvailable(QImage)));
+    faceXObject = new FaceX("model.xml.gz");
+    faceXLandmarks.resize(faceXObject->landmarks_count());
+    openCVcascadeClassifier = new cv::CascadeClassifier("haarcascade_frontalface_alt2.xml");
 }
 
 void FaceAlignmentWidget::paintEvent(QPaintEvent * event)
 {
     QPainter painter(this);
     painter.drawImage(0, 0, currentFrame);
+    for (cv::Point2d p : faceXLandmarks)
+    {
+        painter.drawEllipse(p.x, p.y, 4, 4);
+    }
+
 }
 
 void FaceAlignmentWidget::setCamera(QCamera *camera)
@@ -36,6 +45,37 @@ void FaceAlignmentWidget::onFrameAvailable(QImage frame)
 {
     qDebug() << frame.size();
     currentFrame = frame;
+    //cout << "Press \"r\" to re-initialize the face location." << endl;
+    QImage rgbImage = currentFrame.convertToFormat(QImage::Format_RGB888);
+    cv::Mat cvFrame(rgbImage.height(),rgbImage.width(),CV_8UC3,rgbImage.scanLine(0));
+    cv::Mat img;
+
+        cv::cvtColor(cvFrame, img, cv::COLOR_BGR2GRAY);
+        //cv::imshow("Gray image", img);
+
+        std::vector<cv::Point2d> original_landmarks = faceXLandmarks;
+        faceXLandmarks = faceXObject->Alignment(img, faceXLandmarks);
+
+        for (int i = 0; i < faceXLandmarks.size(); ++i)
+        {
+            faceXLandmarks[i].x = (faceXLandmarks[i].x + original_landmarks[i].x) / 2;
+            faceXLandmarks[i].y = (faceXLandmarks[i].y + original_landmarks[i].y) / 2;
+        }
+
+        /*for (cv::Point2d p : landmarks)
+        {
+            cv::circle(frame, p, 1, cv::Scalar(0, 255, 0), 2);
+        }
+
+        cv::imshow("\"r\" to re-initialize, \"q\" to exit", frame);
+        int key = cv::waitKey(10);
+        if (key == 'q')
+            break;
+        else if (key == 'r')
+        {
+
+        }*/
+
     repaint();
 }
 
@@ -50,4 +90,19 @@ void FaceAlignmentWidget::onCameraStateChanged(QCamera::State state)
     viewFinderSettings.setResolution(QSize(320, 240));
     camera->setViewfinderSettings(viewFinderSettings);
     setMinimumSize(320, 240);
+}
+
+void FaceAlignmentWidget::mousePressEvent(QMouseEvent *event)
+{
+    QWidget::mousePressEvent(event);
+    std::vector<cv::Rect> faces;
+    QImage rgbImage = currentFrame.convertToFormat(QImage::Format_RGB888);
+    cv::Mat cvFrame(rgbImage.height(),rgbImage.width(),CV_8UC3,rgbImage.scanLine(0));
+    cv::Mat img;
+    cv::cvtColor(cvFrame, img, cv::COLOR_BGR2GRAY);
+    openCVcascadeClassifier->detectMultiScale(img, faces);
+    if (!faces.empty())
+    {
+        faceXLandmarks = faceXObject->Alignment(img, faces[0]);
+    }
 }
--- FaceAlignmentWidget.h
+++ FaceAlignmentWidget.h
@@ -1,11 +1,14 @@
 #ifndef FACEALIGNMENTWIDGET_H
 #define FACEALIGNMENTWIDGET_H
 
+#include <opencv/cv.hpp>
+#include <vector>
 #include <QCamera>
 #include <QWidget>
 #include "SimpleCameraSurface.h"
 
 class QCamera;
+class FaceX;
 
 class FaceAlignmentWidget : public QWidget
 {
@@ -15,6 +18,10 @@ protected:
     QCamera *camera;
     SimpleCameraSurface *simpleCameraSurface;
     void paintEvent(QPaintEvent * event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    cv::CascadeClassifier *openCVcascadeClassifier;
+    std::vector<cv::Point2d> faceXLandmarks;
+    FaceX *faceXObject;
 public:
     explicit FaceAlignmentWidget(QWidget *parent = 0);
     void setCamera(QCamera *camera);
